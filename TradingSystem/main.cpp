b210653dd91bf69233b70ba193ac145d186489d3
ImYongbin <lyb0203@gmail.com>
2025-03-31 14:36:28 +0900

    [red] test login

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -1,5 +1,21 @@
 #include "gmock/gmock.h"
 
+TEST(TestTradingSystem, TEST_KIWER_LOGIN) {
+	StockBrocker brocker;
+	brocker.selectStockBrocker("KIWER");
+	AutoTradingSystem tradingSystem{ &brocker };
+	
+	EXPECT_NO_THROW(brocker.login("USER", "PASSWORD"));
+}
+
+TEST(TestTradingSystem, TEST_NEMO_LOGIN) {
+	StockBrocker brocker;
+	brocker.selectStockBrocker("NEMO");
+	AutoTradingSystem tradingSystem{ &brocker };
+
+	EXPECT_NO_THROW(brocker.certification("USER", "PASSWORD"));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
