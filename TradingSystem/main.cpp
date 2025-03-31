51a2fb8d5a38d40d498831184e30b4265c9dcb52
ImYongbin <lyb0203@gmail.com>
2025-03-31 16:11:08 +0900

    [red] buy, sell unittest 추가

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -1,5 +1,23 @@
 #include "gmock/gmock.h"
 #include "AutoTradingSystem.cpp"
+#include <string>
+using namespace std;
+
+class KiwerMock : public StockBrocker {
+public:
+	MOCK_METHOD(void, login, (string id, string pass), (override));
+	MOCK_METHOD(void, buy, (string code, int price, int count), (override));
+	MOCK_METHOD(void, sell, (string code, int price, int count), (override));
+	MOCK_METHOD(int, getPrice, (string code), (override));
+};
+
+class NemoMock : public StockBrocker {
+public:
+	MOCK_METHOD(void, login, (string id, string pass), (override));
+	MOCK_METHOD(void, buy, (string code, int price, int count), (override));
+	MOCK_METHOD(void, sell, (string code, int price, int count), (override));
+	MOCK_METHOD(int, getPrice, (string code), (override));
+};
 
 TEST(TestTradingSystem, TEST_KIWER_LOGIN) {
 	AutoTradingSystem autoSystem;
@@ -12,6 +30,44 @@ TEST(TestTradingSystem, TEST_NEMO_LOGIN) {
 	autoSystem.selectStockBrocker("NEMO");
 	EXPECT_NO_THROW(autoSystem.login("USER", "PASSWORD"));
 }
+
+TEST(TestTradingSystem, TEST_KIWER_BUY) {
+	AutoTradingSystem autoSystem;
+	autoSystem.selectStockBrocker("KIWER");
+	KiwerMock testMock;
+	EXPECT_CALL(testMock, buy("SAMSUNG", 10000, 3)).Times(1);
+
+	autoSystem.buy("SAMSUNG", 10000, 3);
+}
+
+TEST(TestTradingSystem, TEST_NEMO_BUY) {
+	AutoTradingSystem autoSystem;
+	autoSystem.selectStockBrocker("NEMO");
+	NemoMock testMock;
+	EXPECT_CALL(testMock, buy("SAMSUNG", 10000, 3)).Times(1);
+
+	autoSystem.buy("SAMSUNG", 10000, 3);
+}
+
+TEST(TestTradingSystem, TEST_KIWER_SELL) {
+	AutoTradingSystem autoSystem;
+	autoSystem.selectStockBrocker("KIWER");
+	KiwerMock testMock;
+	EXPECT_CALL(testMock, sell("SAMSUNG", 12000, 3)).Times(1);
+
+	autoSystem.sell("SAMSUNG", 12000, 3);
+}
+
+TEST(TestTradingSystem, TEST_NEMO_SELL) {
+	AutoTradingSystem autoSystem;
+	autoSystem.selectStockBrocker("NEMO");
+	NemoMock testMock;
+	EXPECT_CALL(testMock, sell("SAMSUNG", 12000, 3)).Times(1);
+
+	autoSystem.sell("SAMSUNG", 12000, 3);
+}
+
+
 
 int main() {
 	::testing::InitGoogleMock();
